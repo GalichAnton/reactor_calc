35544c8f25fa4f49594b9ec61cb07ecf93605f52
anton_galich <inthesky133@gmail.com>
2024-10-18 16:02:08 +0300

    add measurments

--- src/components/form/AppForm.tsx
+++ src/components/form/AppForm.tsx
@@ -102,6 +102,7 @@ const AppForm = () => {
                                 placeholder={'Мощность номинальная'}
                                 value={nominalPower}
                                 onChange={onChangeNominalPowerHandler}
+                                addonAfter={'Вт'}
                             />
                         </Form.Item>
                     </Col>
@@ -129,6 +130,7 @@ const AppForm = () => {
                                 placeholder={'Мощность'}
                                 disabled={true}
                                 value={power}
+                                addonAfter={'Вт'}
                             />
                         </Form.Item>
                     </Col>
